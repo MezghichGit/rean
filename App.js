547222fa2e344b6a63e4c4ee497fb57f2bd30753
moahmed amine mezghich <git@mezghich.com>
2022-09-14 12:05:50 +0100

    exemple Hello World avec useState

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button,ActivityIndicator } from 'react-native';
+import React, { useState } from "react";
+
+function traitement(){
+  console.log("Récuperattion de Data...");
+}
+
+
+
 
 export default function App() {
+
+  const [titleText, setTitleText] = useState("React Native");
+
+  const onPressTitle = () => {
+    setTitleText("Mise à jour du text");
+  };
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      
+          <Text style={styles.textStyle} onPress={onPressTitle}>{titleText}</Text>
+    
+
+      <Button
+        title="Press me"
+        onPress={traitement}
+        color ='blue'
+      />
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +39,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    //flexDirection:'col',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  child1: {
+    flex: 0.3,
+    backgroundColor: '#68F74F',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+,
+child2: {
+  flex: 0.4,
+  backgroundColor: '#F7F44F',
+  alignItems: 'center',
+  justifyContent: 'center',
+},
+textStyle: {
+  fontSize:20
+}
 });
